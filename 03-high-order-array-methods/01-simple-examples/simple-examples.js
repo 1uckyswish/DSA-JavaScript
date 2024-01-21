8fdd59e201a6e9b312248eaf3bf52e3228b7f016
Noel Guillen <noelblas274@gmail.com>
2024-01-20 20:21:58 -0500

    start of higher order functions

--- 03-high-order-array-methods/01-simple-examples/simple-examples.js
+++ 03-high-order-array-methods/01-simple-examples/simple-examples.js
@@ -4,16 +4,26 @@ const numbers = [1, 2, 3, 4, 5];
  * map: Transforms array elements with a provided function, creating a new array.
  */
 
+const dbl = numbers.map((item, index, arr)=>{
+    return item * 2 ;
+})
 
 /**
  * filter: Creates a new array with elements that satisfy a specified condition.
  */
+
+const evenNum = numbers.filter((num) => num === 5);
 
 
 
 /**
  * reduce: Accumulates array elements into a single value using a provided function.
  */
+
+const sum = numbers.reduce((total, start)=>{
+    return total + start;
+}, 0)
+
 
 
 
@@ -22,10 +32,20 @@ const numbers = [1, 2, 3, 4, 5];
  */
 
 
+// numbers.forEach((num, index)=>{
+//     console.log(num[index])
+// })
+
 
  /**
  * find: Returns the first array element that satisfies a specified condition.
  */
+
+const found = numbers.find((num)=>{
+    return num % 2 === 0;
+})
+
+console.log(found);
 
 
 /**
--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,11 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(array) {
+    const filterNumbs = array.filter((num)=> num % 2 === 0);
+    const squareNumbs = filterNumbs.map((num) => num * num);
+    const sum = squareNumbs.reduce((total, start) => {
+        return total + start;
+    }, 0)
+
+    return sum;
+}
 
 module.exports = sumOfEvenSquares;
