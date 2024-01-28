cd0d12b0bf62cbb9bcba14c75d4251e48d25c8e4
Noel Guillen <noelblas274@gmail.com>
2024-01-27 19:33:11 -0500

    final challenge of higher order functions

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,16 @@
-function validatePassword() {}
+function validatePassword(password) {
+    const lengthValue = password.length >= 8;
+    //*SPLIT THE WORD INTO AN ARRAY, THEN USE SOME TO CHECK IF ONE CONDITION IS TRUE, IF ONE IS CAPS AND ONE IS NOT LOWER
+    const hasUpperCase = password.split("").some((char)=> char === char.toUpperCase() && char !== char.toLowerCase());
+    const hasLowerCase = password.split("").some((char)=> char === char.toLowerCase() && char !== char.toUpperCase());
+    const hasDigit = password.split("").some((char)=>  !isNaN(parseInt(char, 10)));
 
+    return lengthValue && hasUpperCase && hasLowerCase && hasDigit;
+}
+
+
+const result1 = validatePassword('Abc12345');
+const result2 = validatePassword('password');
+
+console.log(result1, result2);
 module.exports = validatePassword;
