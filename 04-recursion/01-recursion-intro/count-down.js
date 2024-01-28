4dcf2732321c1e0803faa0b317f09133fc9d2400
Noel Guillen <noelblas274@gmail.com>
2024-01-27 19:50:19 -0500

    start of recursive functions

--- 04-recursion/01-recursion-intro/count-down.js
+++ 04-recursion/01-recursion-intro/count-down.js
@@ -1,3 +1,15 @@
-function countDown() {}
+function countDown(num) {
+    //base case
+
+    if(num <= 0){
+        console.log("All done");
+        return;
+    }
+
+    console.log(num);
+    num--;
+    countDown(num);
+    //recursive case
+}
 
 module.exports = countDown;
