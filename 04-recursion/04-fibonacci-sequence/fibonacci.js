f6833210233fe478d64835368864766d7e0ba1df
Noel Guillen <noelblas274@gmail.com>
2024-01-29 18:52:32 -0500

    factorial set

--- 04-recursion/04-fibonacci-sequence/fibonacci.js
+++ 04-recursion/04-fibonacci-sequence/fibonacci.js
@@ -1,3 +1,13 @@
-function fibonacci() {}
+function fibonacci(num) {
+    if(num < 2){
+        return num;
+    }
+
+    return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+const result = fibonacci(5);
+
+console.log(result);
 
 module.exports = fibonacci;
--- 04-recursion/05-factorial/factorial.js
+++ 04-recursion/05-factorial/factorial.js
@@ -1,3 +1,14 @@
-function factorial() {}
+function factorial(num) {
+    if(num === 0 || num === 1){
+        return 1;
+    }
+
+    return num * factorial(num - 1)
+}
+
+
+const result = factorial(5);
+
+console.log(result);
 
 module.exports = factorial;
