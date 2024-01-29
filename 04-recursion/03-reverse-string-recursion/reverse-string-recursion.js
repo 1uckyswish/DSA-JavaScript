30c472f4bab3f713761bdca3bfc15fe1a67fd32e
Noel Guillen <noelblas274@gmail.com>
2024-01-29 18:27:06 -0500

    recurrsion unwind

--- 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
+++ 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
@@ -1,3 +1,15 @@
-function reverseString() {}
+function reverseString(str) {
+    if(str === ''){
+        return '';
+    }
+
+    return reverseString(str.substr(1)) + str[0];
+}
+
+
+const result = reverseString('hello');
+
+console.log(result);
+
 
 module.exports = reverseString;
